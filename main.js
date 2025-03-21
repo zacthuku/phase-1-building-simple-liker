9fbe7752f2f18c3ef5aca5f0913dbc2536d4c67e
Zacharia Ndinguri <zacthuku@gmail.com>
2025-03-21 08:29:56 +0300

    liker lab

--- main.js
+++ main.js
@@ -3,7 +3,43 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const errorModal = document.getElementById("error-modal");
+const modalMessage = document.getElementById("modal-message");
 
+const likeGlyphs = document.querySelectorAll(".like-glyph");
+likeGlyphs.forEach(glyph => {
+  glyph.addEventListener("click", () => {
+    mimicServerCall()
+      .then(() => {
+        if (glyph.innerText === EMPTY_HEART) {
+          glyph.innerText = FULL_HEART;
+          glyph.classList.add("activated-heart");
+        } else {
+          glyph.innerText = EMPTY_HEART;
+          glyph.classList.remove("activated-heart");
+        }
+      })
+      .catch(error => {
+     
+        modalMessage.innerText = error;
+        errorModal.classList.remove("hidden");
+
+        
+        setTimeout(() => {
+          errorModal.classList.add("hidden");
+        }, 3000);
+      });
+  });
+});
+
+function mimicServerCall(url = "http://mimicServer.example.com", config = {}) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+     
+      Math.random() < 0.2 ? reject("Random server error. Try again!") : resolve("Pretend remote server notified of action!");
+    }, 300);
+  });
+}
 
 
 
